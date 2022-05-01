ede585d7ee514508e56c9af92efc133f3cb9bf43
Ankit <ankit.kumar1@tokopedia.com>
2022-05-01 19:04:44 +0530

    initial setup

---
+++ app/src/main/java/lostankit7/android/timerappcompose/MainActivity.kt
@@ -0,0 +1,20 @@
+package lostankit7.android.timerappcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import lostankit7.android.timerappcompose.ui.theme.TimerAppComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
