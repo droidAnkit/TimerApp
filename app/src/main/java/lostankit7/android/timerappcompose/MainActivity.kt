76c710ff9016b756c627a965536d1a32b283a06e
Ankit <ankit.kumar1@tokopedia.com>
2022-05-03 22:25:10 +0530

    timer compose

--- app/src/main/java/lostankit7/android/timerappcompose/MainActivity.kt
+++ app/src/main/java/lostankit7/android/timerappcompose/MainActivity.kt
@@ -3,18 +3,32 @@ package lostankit7.android.timerappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import lostankit7.android.timerappcompose.ui.theme.TimerAppComposeTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import lostankit7.android.timerappcompose.ui.compose.Timer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize().background(Color(0xFF101010))
+            ) {
+                Timer(
+                    totalTime = 100L * 1000L,
+                    handleColor = Color.Yellow,
+                    inActiveBarColor = Color.DarkGray,
+                    activeBarColor = Color(0xFF37B900),
+                    modifier = Modifier.size(200.dp)
+                )
+            }
         }
     }
 }
